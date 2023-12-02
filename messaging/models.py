817609048c6fecaec51a4b9f8ece9c96bcccd069
theakif <akif.hussaink@gmail.com>
2023-12-03 04:07:04 +0500

    Added docstrings for models, serializers and views.

--- messaging/models.py
+++ messaging/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Message(models.Model):
+    """
+    Repository for message.
+    """
+
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        This function shows a str name on the admin panel instead of showing like 'Object(1)'.
+        """
         return f"{self.sender} - {self.content[:50]}"
