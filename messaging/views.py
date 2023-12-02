509e446d5bda98f694e648c992ab4e2d81d15e9c
theakif <akif.hussaink@gmail.com>
2023-12-03 03:54:55 +0500

    Added Message model, serializers and viewset.

--- messaging/views.py
+++ messaging/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Message
+from .serializers import MessageSerializer
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
