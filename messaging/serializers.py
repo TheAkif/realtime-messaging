d3f82324e9a1bd8ab2d9b935aee3548d72e2081f
theakif <akif.hussaink@gmail.com>
2023-12-03 04:07:04 +0500

    Added docstrings for models, serializers and views.

--- messaging/serializers.py
+++ messaging/serializers.py
@@ -2,7 +2,12 @@
 from rest_framework import serializers
 from .models import Message
 
+
 class MessageSerializer(serializers.ModelSerializer):
+    """
+    Viewset to convert the Message model instances into JSON and vice versa.
+    """
+
     class Meta:
         model = Message
-        fields = ['id', 'sender', 'content', 'timestamp']
+        fields = ["id", "sender", "content", "timestamp"]
--- messaging/views.py
+++ messaging/views.py
@@ -5,7 +5,14 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Message
 from .serializers import MessageSerializer
 
+
 class MessageViewSet(viewsets.ModelViewSet):
+    """
+    Viewset to handle CRUD operation for Message model.
+    Includes serializer class to serialize data and permission 
+    class to handle if the request is authenticated or not.
+    """
+
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
