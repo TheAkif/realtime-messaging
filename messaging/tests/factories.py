c82f025ff566ae240fcf7c4090049791eb99ce5a
theakif <akif.hussaink@gmail.com>
2023-12-03 05:48:19 +0500

    Added pytest and wrote test cases for Message model.

---
+++ messaging/tests/factories.py
@@ -0,0 +1,10 @@
+# yourapp/tests/factories.py
+import factory
+from django.contrib.auth.models import User
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f"user{n}")
+    email = factory.LazyAttribute(lambda o: f"{o.username}@example.com")
