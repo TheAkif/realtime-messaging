038dee067f0cc8296b85220509886c13f5963c11
theakif <akif.hussaink@gmail.com>
2023-12-03 06:34:34 +0500

    Setup ASGI server to run websockets on django.

--- realtime_messaging_project/asgi.py
+++ realtime_messaging_project/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from messaging.routing import websocket_urlpatterns  # Import your app's WebSocket routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realtime_messaging_project.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realtime_messaging_project.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
