936cf1eab69750ac92073d7c4176c70f02a4dd7c
theakif <akif.hussaink@gmail.com>
2023-12-04 05:11:25 +0500

    Setup frontend on react-redux to handle user login and registration.

---
+++ frontend/chat/src/config/index.js
@@ -0,0 +1,4 @@
+export const API_URL = process.env.REACT_APP_API_URL;
+// export const API_URL_PROD = process.env.REACT_APP_API_URL_PROD;
+// export const API_URL_TEST = process.env.REACT_APP_API_URL_TEST;
+// export const API_URL_DEV = process.env.REACT_APP_API_URL_DEV;
---
+++ frontend/chat/src/containers/RegisterPage.js
@@ -0,0 +1,11 @@
+import Layout from "components/Layout";
+
+const RegisterPage = () => {
+    return (
+        <Layout title='Real-time Chat | Register' content='Register page.'>
+            <h1>Register Page</h1>
+        </Layout>
+    );
+};
+
+export default RegisterPage;
