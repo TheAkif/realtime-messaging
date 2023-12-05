387b267dbcbd8239c7533ad12dd0abec94216ad6
theakif <akif.hussaink@gmail.com>
2023-12-06 03:21:23 +0500

    Login, signup, cookies setting, access and refresh token setting in cookies and integration with the backend.

--- backend/messaging/urls.py
+++ backend/messaging/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from messaging.views import MessageViewSet, RegisterViewSet, RetrieveUserViewSet
 
-router = DefaultRouter()
-router.register(r'messages', MessageViewSet)
+# router = DefaultRouter()
+# router.register(r"messages", MessageViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('register', RegisterViewSet.as_view()),
-    path('user', RetrieveUserViewSet.as_view())
+    # path("", include(router.urls)),
+    path("register", RegisterViewSet.as_view()),
+    path("me", RetrieveUserViewSet.as_view()),
 ]
