23912bafbcad6462cf63ec52de5f76bbe6113ab3
theakif <akif.hussaink@gmail.com>
2023-12-09 05:20:58 +0500

    Timestamp showing changes on chat page.

--- frontend/client/src/containers/ChatPage.js
+++ frontend/client/src/containers/ChatPage.js
@@ -64,7 +64,7 @@ const ChatPage = () => {
 	// ... (rest of your imports and component setup)
 
 	return (
-		<Layout title='Realtime messaging | chat' content='chat page'>
+		<Layout title='Realtime messaging | Chat' content='Chat page'>
 			{loading ? (
 				<div className='d-flex justify-content-center align-items-center' style={{ height: '100vh' }}>
 					<div className='spinner-border text-primary' role='status'>
@@ -72,11 +72,11 @@ const ChatPage = () => {
 					</div>
 				</div>
 			) : (
-				<div className='container'>
-					<div className='row' style={{ height: '80vh' }}>
+				<div className='container py-4'>
+					<div className='row no-gutters' style={{ height: '80vh' }}>
 						{/* User List Sidebar */}
-						<div className='col-4 border-end'>
-							<div className='list-group'>
+						<div className='col-md-4 col-lg-3 bg-light border-right'>
+							<div className='list-group list-group-flush'>
 								{users.map((chatUser) => (
 									<button
 										key={chatUser.id}
@@ -90,32 +90,46 @@ const ChatPage = () => {
 							</div>
 						</div>
 						{/* Chat Area */}
-						<div className='col-8'>
-							{activeChatUser ? (
-								<>
-									<div className='chat-messages overflow-auto mb-3' style={{ maxHeight: '70%' }}>
-										{chatHistory.map((message, index) => (
-											<div key={index} className={`m-2 d-flex ${message.sender === user.id ? 'justify-content-end' : 'justify-content-start'}`}>
-												<div className={`badge ${message.sender === user.id ? 'bg-primary' : 'bg-secondary'} pl-2 pr-2 pb-3 pt-3`}>
+						<div className='col-md-8 col-lg-9'>
+							<div className='chat-header p-3 border-bottom'>
+								<strong>{activeChatUser ? `${activeChatUser.first_name} ${activeChatUser.last_name}` : 'Chat'}</strong>
+							</div>
+							<div className='chat-messages p-3 overflow-auto' style={{ maxHeight: '70%' }}>
+								{activeChatUser ? (
+									chatHistory.length > 0 ? (
+										chatHistory.map((message, index) => (
+											<div key={index} className={`message mb-3 d-flex flex-column ${message.sender === user.id ? 'align-items-end' : 'align-items-start'}`}>
+												<div className={`d-inline-block p-2 rounded ${message.sender === user.id ? 'bg-primary text-white' : 'bg-light'}`}>
 													{message.content}
 												</div>
+												<div className='text-muted small mt-1' style={{ fontSize: '0.75rem' }}>
+													{new Date(message.timestamp).toLocaleDateString()} {new Date(message.timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+												</div>
 											</div>
-										))}
-									</div>
-									<div className='chat-input'>
-										<input className='form-control' type='text' placeholder='Type a message...' />
-										<button className='btn btn-primary mt-2'>Send</button>
-									</div>
-								</>
-							) : (
-								<p className='text-center align-middle my-auto'>Select a user to start chatting</p>
+										))
+									) : (
+										<div className='text-center text-muted'>
+											<p>No messages yet. Start the conversation!</p>
+										</div>
+									)
+								) : (
+									<p className='text-center my-auto'>Select a user to start chatting</p>
+								)}
+							</div>
+							{activeChatUser && (
+								<div className='chat-input p-3 border-top'>
+									<input className='form-control' type='text' placeholder='Type a message...' />
+									<button className='btn btn-primary mt-2 float-right'>Send</button>
+								</div>
 							)}
 						</div>
+
 					</div>
 				</div>
 			)}
 		</Layout>
 	);
+
 };
 
 export default ChatPage;
